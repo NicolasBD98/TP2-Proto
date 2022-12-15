289fe5c816bd821a92adb9b8bd331ce6153a0106
Marie G <guillot-marie@hotmail.fr>
2022-12-15 14:36:50 -0500

    Camera

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -26,7 +26,7 @@ public class CameraMovement : MonoBehaviour
         Vector3 targetPosition = player.transform.position + cameraOffset; // Recule la caméra hors du plan du jeu, pour qu'elle "filme" le jeu.
         Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.deltaTime); //La position de la caméra est interpolée pour qu'elle ne suive pas directement le joueur. 
         //float clampX = Mathf.Clamp(smoothPosition.x, 0f, 100f); // Permet de restreindre la caméra aux limites du terrain si nécessaire. 
-        float clampY = Mathf.Clamp(smoothPosition.y, 0f, 20f);
-        transform.position = new Vector3(smoothPosition.x, clampY, -10f);
+        //float clampY = Mathf.Clamp(smoothPosition.y, 0f, 20f);
+        transform.position = new Vector3(smoothPosition.x, smoothPosition.y, -10f);
     }
 }
